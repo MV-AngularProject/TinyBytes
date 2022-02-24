b5d7c892400202aac6edfe17fe6d7e49c5874b39
Dinalisse Felicione <dinalisse@google.com>
2022-02-24 19:04:24 +0000

    add starting files

---
+++ tinyBytes/src/app/categories/categories.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './categories.component.html'
+})
+export class WelcomeComponent {
+  public pageTitle = 'Category page';
+}

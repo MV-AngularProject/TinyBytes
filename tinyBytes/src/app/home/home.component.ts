9a1cf2fdd6765af6211f0f4bd73ffe01ce4cafee
Dinalisse Felicione <dinalisse@google.com>
2022-02-25 08:53:51 +0000

    homepage set up

--- tinyBytes/src/app/home/home.component.ts
+++ tinyBytes/src/app/home/home.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IRecipe } from '../interface/recipe';
+import { RecipeService } from '../service/recipe.service';
 
 @Component({
+  selector: 'home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
-  public pageTitle = 'Welcome';
+export class HomeComponent implements OnInit, OnDestroy {
+  errorMessage: string ='';
+  sub!: Subscription;
+  
+  constructor(private recipeService: RecipeService){}
+
+  recipes : IRecipe[] =[];
+
+  ngOnInit(): void {
+      this.recipeService.getRecipes().subscribe({
+        next: recipes => this.recipes =recipes,
+        error: err => this.errorMessage =err
+      });
+  }
+
+  ngOnDestroy() {
+      this.sub.unsubscribe();
+  }
 }

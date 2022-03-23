14925fbd4103b5b3d1763644c89a6a2187fe31d6
andersonquinones <anderson.quinones10@gmail.com>
2022-03-23 20:56:59 +0000

    Implementing API

--- tinyBytes/src/app/service/searchResults.service.ts
+++ tinyBytes/src/app/service/searchResults.service.ts
@@ -17,6 +17,14 @@ export class SearchService{
         )
     }
 
+    category(query:string): Observable<ISearchResults>{
+        return this.http.get<ISearchResults>(`https://api.spoonacular.com/recipes/complexSearch?apiKey=b989a147ccb6450e920e8fa5355c632c&query=${query}&number=8`).pipe(
+            tap(data => console.log('All', JSON.stringify(data.results), "value", (document.getElementById('category')as HTMLInputElement).value)),
+            catchError(this.handleError)
+        )
+    }
+
+
     private handleError(err:HttpErrorResponse){
         let errorMessage='';
         if( err.error instanceof ErrorEvent){

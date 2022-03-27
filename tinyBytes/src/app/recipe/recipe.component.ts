087123feaddfe746fc5efd2be399932bc9086b6e
Jorge F <jorge.m.flores@protonmail.com>
2022-03-27 04:32:22 +0000

    styling, icons, front interaction functionality

--- tinyBytes/src/app/recipe/recipe.component.ts
+++ tinyBytes/src/app/recipe/recipe.component.ts
@@ -4,30 +4,60 @@ import { Subscription } from 'rxjs';
 import { RecipeDetailsService } from './recipeDetails.service';
 import { IRecipeDetails, IInstructions } from '../interface/recipeDetails';
 import { IHttpError } from '../interface/error';
+import { IUserData } from '../interface/userData';
 
 @Component({
   templateUrl: './recipe.component.html',
   styleUrls: ['recipe.component.css'],
 })
+
 export class RecipeComponent implements OnInit, OnDestroy, AfterViewInit {
   constructor(
     private route: ActivatedRoute,
     private recipeDetailsService: RecipeDetailsService
   ) {}
 
+  //Subscription Variables
   detailsSub!: Subscription;
   instructionsSub!: Subscription;
   nutritionSub!: Subscription;
-  
+
+  //Variables
   recipeId!: string | null;
   details!: IRecipeDetails;
   instructions!: IInstructions[];
   nutrition!: string | IHttpError;
+  userData: IUserData = { favorited: false };
+  team: string[] = [
+    'Jorge Flores',
+    'Denille Carrington',
+    'Anderson Quinones',
+    'Dinalisse Felicone',
+  ];
+  author!: string;
 
+  //DOM
   @ViewChild('divNutritionHtml') divNutritionHtmlRef!: ElementRef;
+  @ViewChild('divRecipeDescription') divRecipeDescriptionRef!: ElementRef;
 
+  //Custom Functions
+  randomize(arr: string[]): string {
+    return arr[Math.floor(Math.random() * 4)];
+  }
+  printRecipe(): void {
+    window.print();
+  }
+  bookmarkRecipe(): void {
+    this.userData.favorited = !this.userData.favorited;
+  }
+
+  //Life Cycle
   ngOnInit(): void {
+    //Initializations
     this.recipeId = this.route.snapshot.paramMap.get('recipeId');
+    this.author = this.randomize(this.team);
+
+    //Subscriptions
     this.detailsSub = this.recipeDetailsService
       .getRecipeDetails(this.recipeId)
       .subscribe({
@@ -35,7 +65,7 @@ export class RecipeComponent implements OnInit, OnDestroy, AfterViewInit {
           console.log('Details Data: ', details);
           this.details = <IRecipeDetails>details;
         },
-        error: (err: IHttpError) => console.error(err.detailedMessage)
+        // error: (err: IHttpError) => console.error(err.detailedMessage),
       });
     // No instruction data for some recipeids, must have html response in those situations
     this.instructionsSub = this.recipeDetailsService
@@ -45,22 +75,33 @@ export class RecipeComponent implements OnInit, OnDestroy, AfterViewInit {
           console.log('Instructions Data: ', <IInstructions[]>instructions);
           this.instructions = <IInstructions[]>instructions;
         },
-        error: (err: IHttpError) => console.error(err.detailedMessage)
+        // error: (err: IHttpError) => console.error(err.detailedMessage),
       });
-      this.nutritionSub = this.recipeDetailsService
+    this.nutritionSub = this.recipeDetailsService
       .getHTMLNutritionFacts(this.recipeId)
       .subscribe({
         next: (nutritionHtml: string | IHttpError) => {
           console.log('Nutrition HTML: ', nutritionHtml);
           this.nutrition = <string>nutritionHtml;
-        }, 
-        error: (err: IHttpError) => console.error(err.detailedMessage)
-      })
+        },
+        error: (err: IHttpError) => {
+          console.error('Status Code: ', err.statusCode)
+          console.error('Detailed Message: ', err.detailedMessage)
+          console.error('Component Message: ', err.componentMessage)
+          console.error('Status Text: ', err.statusText)
+          console.error('Data Type: ', err.dataType)
+        },
+      });
   }
 
   ngAfterViewInit(): void {
-    if(this.divNutritionHtmlRef.nativeElement) {
+    //DOM Manipulation
+    if (this.divNutritionHtmlRef.nativeElement) {
       this.divNutritionHtmlRef.nativeElement.innerHTML = this.nutrition;
+    }
+    if (this.divRecipeDescriptionRef.nativeElement) {
+      this.divRecipeDescriptionRef.nativeElement.innerHTML =
+        this.details.summary;
     }
   }
 

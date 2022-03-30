431b92fe18274429beaeecf500e67e3d08b33174
CDenille <carringtondenille@gmail.com>
2022-03-30 14:58:33 +0000

    media query for footer

--- tinyBytes/src/app/service/recipe.service.ts
+++ tinyBytes/src/app/service/recipe.service.ts
@@ -9,8 +9,8 @@ import { Root } from "../interface/root";
 })
 export class RecipeService{
     constructor(private http : HttpClient){}
-    //apiKey ='&apiKey=dd0d974a8e534716a3175c56ecd0bde5'
-    apiKey ='&apiKey=db3f5aecd2e84a9ea1773b57ca6373f7'
+    apiKey ='&apiKey=dd0d974a8e534716a3175c56ecd0bde5'
+    // apiKey ='&apiKey=db3f5aecd2e84a9ea1773b57ca6373f7'
     // apiKey ='&apiKey=f082f3f33d8e400b8898966f7fcbc069'
     // apiKey ='&apiKey=0550322f781e49199dd00666b1933e64'
 

e5ba68f56d5f865288374c29afb1172cc836f5ae
Jorge F <jorge.m.flores@protonmail.com>
2022-05-05 18:45:10 +0000

    seed relationship, alter associations

--- db/associations.js
+++ db/associations.js
@@ -2,9 +2,6 @@ const User = require('./models/User');
 const Recipe = require('./models/Recipe');
 const db = require('./db');
 
-User.hasMany(Recipe);
-Recipe.belongsTo(User);
-
 User.belongsToMany(Recipe, {through: 'Favorites'});
 Recipe.belongsToMany(User, {through: 'Favorites'});
 

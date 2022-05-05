589fc371f365f615d2b4793f4e1fd15ab7734ade
CDenille <carringtondenille@gmail.com>
2022-05-05 13:27:41 +0000

    added search route to backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 const PORT = 8080;
 
 // const {User, Recipe}= require('./db/associations');
@@ -21,6 +22,30 @@ console.log('after')
     
 //     res.status(200).send({message: 'Successfully signed up'})
 // })
-app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
-});
+
+// app.use('/',require('./api'))
+
+app.get('/search/:query', function (req, res) {
+  request({
+    method: 'GET',
+    uri: `https://api.spoonacular.com/recipes/complexSearch?apiKey=a42bca2f8c2f4c5194cd8aa86c365de7&query=${req.params.query}&number=8`,
+  }, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const data = response.body;
+    const apiData = JSON.parse(data)
+    console.log('Returned: ', apiData)
+    if (response.statusCode == 200) {
+      console.log('success');
+    }
+    else {
+      console.log("error with api call")
+    }
+    }). pipe(res)
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+  });
